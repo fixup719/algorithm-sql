fbda42d8c47bb273bc6ed6a97765ed09b927e7eb
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2023-07-30 22:20:58 +0900

    [Silver III] Title: 주유소, Time: 216 ms, Memory: 20172 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -5,35 +5,17 @@ let line = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 let N = Number(line[0]);
 let dists = line[1].split(" ").map(Number);
 let prices = line[2].split(" ").map(Number);
-let minCity = {
-  city: undefined,
-  price: 100001,
-}
-prices.map((el, idx) => {
-  if(idx !== prices.length-1){
-    if(minCity.price > el){
-      minCity.city = idx;
-      minCity.price = el;
-    }
-  }
-})
-
-
-
 let answer = 0;
-for(let i=0; i<dists.length; i++){
-  if(i===minCity.city){
-    // 가장 적은 기름값의 도시 => 여기서 남은 거리만큼 충전
-    let remain = 0;
-    for(let j=i; j<dists.length; j++){
-        remain += dists[j];
-    }
-    answer += remain*minCity.price;
-    break;
-  }else{
-    // 가장 적은 기름값 도시가 아니라면 => 다음 도시 거리까지만 충전
-    answer += dists[i]*prices[i]
-  }
+
+let minPrice = prices[0];
+for(let i=0; i<N; i++){
+  minPrice = Math.min(minPrice, prices[i]);
+  prices[i] = minPrice;
 }
+
+for(let i=0; i<N-1; i++){
+  answer += dists[i]*prices[i];
+}
+
 
 console.log(answer)
