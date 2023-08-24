fe74138dda6705af697f916901b962f8e4c2d69d
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2023-08-24 15:30:19 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 600 ms, Memory: 100024 KB -BaekjoonHub

--- 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.js
+++ 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.js
@@ -6,34 +6,18 @@ let [N, M] = line[0].split(" ").map(Number);
 let arr1 = line.slice(1, N + 1).map(Number).sort((a, b) => a - b);
 let arr2 = line.slice(N + 1).map(Number);
 
-
-function check(start, end, arr, target) {
-  if (start > end) return -1;
-
-  let mid = parseInt((start + end) / 2);
-
-  if (arr[mid] === target) return 1;
-  else if (arr[mid] > target) return check(start, mid - 1, arr, target);
-  else return check(mid + 1, end, arr, target);
-}
-
 function lowerBound(start, end, arr, target) {
   while (start < end) {
     let mid = parseInt((start + end) / 2);
     if (arr[mid] >= target) end = mid;
     else start = mid + 1;
   }
-  return end
+  return arr[end] === target ? end :  -1;
 }
 
 let ans = [];
 for (let i = 0; i < M; i++) {
-  if (check(0, N, arr1, arr2[i]) === 1) {
     ans.push(lowerBound(0, N, arr1, arr2[i]))
-  } else {
-    ans.push(-1);
-  }
-
 }
 
 console.log(ans.join("\n"))
