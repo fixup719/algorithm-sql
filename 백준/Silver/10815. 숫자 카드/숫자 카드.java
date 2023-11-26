51db42e5f5670651464c8d6c4f332b6c6d8ca45a
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2023-11-26 21:30:42 +0900

    [Silver V] Title: 숫자 카드, Time: 1164 ms, Memory: 189960 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,5 +1,4 @@
 
-
 import java.io.*;
 import java.util.*;
 
@@ -16,18 +15,12 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        // 숫자 카드 개수 N (1 ~ 5*10^5)
-            // 카드 번호 범위 :  -10^7 ~ 10^7
-        // M (1 ~ 5*10^5)
-            // 숫자 번호 범위 :  -10^7 ~ 10^7
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
-
         int[] cards = new int[N];
         for(int i=0; i<N; i++){
             cards[i] = Integer.parseInt(st.nextToken());
@@ -35,12 +28,14 @@ public class Main {
         Arrays.sort(cards);
 
         int M = Integer.parseInt(br.readLine());
-
+        
         st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         while(M-->0){
             int num = Integer.parseInt(st.nextToken());
-            System.out.print(binarySearch(num, 0, cards.length-1, cards)+" ");
+            sb.append(binarySearch(num, 0, cards.length-1, cards)+" ");
         }
+        System.out.println(sb);
 
     }
 
