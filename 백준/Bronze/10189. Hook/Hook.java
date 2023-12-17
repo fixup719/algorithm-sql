bbefa8fc8bf437422cc9d48c20a29b07dfea1443
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2023-12-17 23:06:01 +0900

    [Bronze V] Title: Hook, Time: 72 ms, Memory: 11220 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,15 @@
+
+
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("#  # #### #### #  #\n#### #  # #  # # #\n#### #  # #  # # #\n#  # #### #### #  #");
+        System.out.println(sb);
+    }
+}
