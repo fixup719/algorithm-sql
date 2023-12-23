9f76a6f86444fdb16fe5d7e8d8e9649df1afe215
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2023-12-23 23:29:55 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 80 ms, Memory: 11880 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -0,0 +1,22 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while(N-->0){
+            String str = br.readLine();
+
+            if(str.length()>=6 && str.length()<=9) sb.append("yes\n");
+            else sb.append("no\n");
+        }
+
+        System.out.println(sb);
+    }
+}
