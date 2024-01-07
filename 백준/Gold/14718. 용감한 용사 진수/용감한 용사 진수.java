3f645c7b08da1ae3a7cace2e4372670c2e251ee7
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-01-07 17:20:16 +0900

    [Gold III] Title: 용감한 용사 진수, Time: 356 ms, Memory: 12528 KB -BaekjoonHub

--- 백준/Gold/14718. 용감한 용사 진수/용감한 용사 진수.java
+++ 백준/Gold/14718. 용감한 용사 진수/용감한 용사 진수.java
@@ -1,49 +1,61 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N, K; // N명의 벙사수, 이겨야할 K명의 병사수
+    static int[][] soldiers;
+    static int[] peek;
+    static int cnt; // 이길 수 있는 병사 수 체크
+    static int answer = Integer.MAX_VALUE; // 필요한 스택 포인트
 
-    static int n, k;
-    static int[][] map;
-    static int[] arr = new int[3];
-    static int answer=Integer.MAX_VALUE;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    static void statCheck(int depth){
+        if(depth == 3){
 
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        map = new int[n][3];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            map[i][0] = Integer.parseInt(st.nextToken());
-            map[i][1] = Integer.parseInt(st.nextToken());
-            map[i][2] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.fill(arr, -1);
-        BT(0);
-        System.out.println(answer);
-    }
-
-    public static void BT(int idx) {
-        if (idx == 3) {
-            int count=0;
-            for (int i = 0; i < n; i++) {
-                if (map[i][0] <= arr[0] && map[i][1] <= arr[1] && map[i][2] <= arr[2]) {
-                    count++;
+            cnt = 0;
+            for (int i = 0; i < N; i++) {
+                if(peek[0] >= soldiers[0][i]
+                    && peek[1] >= soldiers[1][i]
+                    && peek[2] >= soldiers[2][i]){
+                    // 셋 스택 모두 진수가 크거나 같을 때
+                    cnt ++;
                 }
             }
-            if (count == k) {
-                answer = Math.min(answer , arr[0]+arr[1]+arr[2]);
+
+            if(cnt >= K){
+                answer = Math.min(answer, peek[0]+peek[1]+peek[2]);
             }
+
             return;
         }
-        for (int i = 0; i < n; i++) {
-            arr[idx] = map[i][idx];
-            BT(idx+1);
-            arr[idx] = -1;
+
+        for(int i=0; i<N; i++){
+            peek[depth] = soldiers[depth][i];
+            statCheck(depth+1);
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        soldiers = new int[3][N]; // 힘, 민첩, 지능별로 N명의 병사의 스탯 담기
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            soldiers[0][i] = Integer.parseInt(st.nextToken());
+            soldiers[1][i] = Integer.parseInt(st.nextToken());
+            soldiers[2][i] = Integer.parseInt(st.nextToken());
+        }
+
+        peek = new int[3];
+
+        statCheck(0);
+
+        System.out.println(answer);
     }
 }
