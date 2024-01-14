38596986f4f1aaa80fc7bd8992a9634e2343bad6
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-01-14 13:28:31 +0900

    [Gold IV] Title: 2로 몇 번 나누어질까, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.java
+++ 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.java
@@ -1,29 +1,27 @@
-
-
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static long divideByTwoSum(long number){
-        long ans = 0;
-        long cnt = 0;
-        long idx = 1;
-        while (number > 0) {
-            if(number%2==0){
-                cnt = number/2;
-            }else{
-                cnt = number/2+1;
-            }
+    static long func(long num){
+        long sum = 0;
 
-            number -= cnt; // 여기는 계산한 항을 뺴준다고 생각
-            ans += cnt*idx;
-            idx *= 2;
+        // 먼저 홀수개만큼 더하기
+        if (num % 2 == 0) {
+            sum += num / 2;
+        } else {
+            sum += num / 2 + 1;
         }
 
-        return ans;
+        long i = 2;
+        while ((num / i) != 0) {
+            sum += i * ((num - i) / (i * 2) + 1);
+            i *= 2;
+        }
 
+        return sum;
     }
 
     public static void main(String[] args) throws IOException {
@@ -33,6 +31,14 @@ public class Main {
         long A = Long.parseLong(st.nextToken());
         long B = Long.parseLong(st.nextToken());
 
-        System.out.println(divideByTwoSum(B) - divideByTwoSum(A-1));
+
+        // A부터 B까지 모든 합계를 구하면 안된다!!!!!
+        // 홀수는 무조건 1이 나옴
+        // 2 : 2 6 10 14 18 22 26 30 ... (4)
+        // 4 : 4 12 20 28 36 ... (8)
+        // 8 : 8 24 .. (16)
+        // sum = 1*(n/2+1(홀수로 끝날때) or n/2(짝수로끝날 때)) + 2*((n-2)/4+1) + 4*((n-4)/8 + 1) + ...
+
+        System.out.println(func(B) - func(A - 1));
     }
 }
