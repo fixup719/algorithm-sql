56595acf9b6f099daa321ca624879463ac69fff9
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-01-18 19:28:55 +0900

    [Gold V] Title: 용액, Time: 292 ms, Memory: 31280 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] values = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            values[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int s = 0, e = n-1, sum = 0, minDiff = values[s] + values[e];
+        int[] ans = {values[s], values[e]};
+        while (s < e) {
+            sum = values[s] + values[e];
+
+            if (sum == 0) {
+                ans[0] = values[s];
+                ans[1] = values[e];
+                break;
+            }
+
+            if (Math.abs(sum) < Math.abs(minDiff)) {
+                minDiff = sum;
+                ans[0] = values[s];
+                ans[1] = values[e];
+                if(sum < 0) s++;
+                else e--;
+            } else if (Math.abs(sum) > Math.abs(minDiff)) {
+                if(sum < 0) s++;
+                else e--;
+            } else {
+                if(sum < 0) s++;
+                else e--;
+            }
+        }
+
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+}
