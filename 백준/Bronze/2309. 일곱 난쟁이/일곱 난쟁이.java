9267e51934a105cdd428de1306c975ab3a6e064f
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-01-23 13:14:49 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 136 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,44 +1,45 @@
-
-
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int[] dwarfs = new int[9];
         int sum = 0;
-        for(int i=0; i<9; i++){
-            int h = Integer.parseInt(br.readLine());
-            sum += h;
-            dwarfs[i] = h;
+        for (int i = 0; i < 9; i++) {
+            dwarfs[i] = Integer.parseInt(br.readLine());
+            sum += dwarfs[i];
         }
 
         Arrays.sort(dwarfs);
-        int idx1=0, idx2=0;
-        f: for(int i=0; i<9 ;i++){
-            for(int j=i+1; j<9; j++){
-                int tmp = sum;
-                tmp = tmp - (dwarfs[i] + dwarfs[j]);
-                if(tmp == 100){
-                    idx1 = i;
-                    idx2 = j;
-                    break f;
-                }
+
+        int s = 0, e = 8;
+        while (s < e) {
+
+            if (sum - (dwarfs[s] + dwarfs[e]) == 100) {
+                dwarfs[s] = 0;
+                dwarfs[e] = 0;
+                break;
+            } else if (sum - (dwarfs[s] + dwarfs[e]) < 100 ) {
+                e -= 1;
+            } else {
+                s += 1;
             }
+
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<9; i++){
-            if(i!=idx1 && i!=idx2){
+        for (int i = 0; i < 9; i++) {
+            if (dwarfs[i] != 0) {
                 sb.append(dwarfs[i]+"\n");
             }
         }
-        System.out.println(sb);
 
-
-
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
