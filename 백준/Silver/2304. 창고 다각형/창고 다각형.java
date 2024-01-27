9be93a5d9e92a5355ddc4ef79682a30751f2a511
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-01-27 21:46:08 +0900

    [Silver II] Title: 창고 다각형, Time: 132 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,72 +1,43 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
+
 
 public class Main {
-
-    static class Poll implements Comparable<Poll>{
-        int x;
-        int y;
-
-        Poll(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public int compareTo(Poll o){
-            return x-o.x;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        Poll[] polls = new Poll[N];
-        for(int i=0; i<N; i++){
+
+        StringTokenizer st;
+        int[] polls = new int[1010];
+        int x, y;
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            polls[i] = new Poll(x, y);
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+
+            polls[x] = y;
         }
 
-        Arrays.sort(polls);
-        int maxH = 0;
-        int maxIdx = 0;
-        for(int i=0; i<N; i++){
-            Poll p = polls[i];
-            if(maxH <= p.y){
-                maxH = p.y;
-                maxIdx = i;
-            }
+        int[] prefix = new int[1010];
+        for (int i = 1; i < 1010; i++) {
+            prefix[i] = Math.max(prefix[i - 1], polls[i]);
+        }
+
+        int[] suffix = new int[1010];
+        for (int i = 1008; i >= 1; i--) {
+            suffix[i] = Math.max(suffix[i + 1], polls[i]);
         }
 
         int sum = 0;
-        int curL=polls[0].x, curH=polls[0].y;
-        for(int i=0; i<=maxIdx; i++){
-            Poll p = polls[i];
-
-            if(curH <= p.y){
-                sum += (p.x-curL)*curH;
-                curL = p.x;
-                curH = p.y;
-            }
+        for (int i = 0; i < 1010; i++) {
+            sum += Math.min(prefix[i], suffix[i]);
         }
 
-
-        curL=polls[N-1].x;
-        curH=polls[N-1].y;
-        for(int i=N-1; i>=maxIdx; i--){
-            Poll p = polls[i];
-
-            if(curH <= p.y){
-                sum += (curL-p.x)*curH;
-                curL = p.x;
-                curH = p.y;
-            }
-        }
-
-        System.out.println(sum + maxH);
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
