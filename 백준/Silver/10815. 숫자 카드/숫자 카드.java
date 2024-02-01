ae080af1062b8707e1a8c91c0d4e6feda6455daa
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-01 18:01:53 +0900

    [Silver V] Title: 숫자 카드, Time: 860 ms, Memory: 174216 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,53 +1,30 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
 public class Main {
 
-    static int binarySearch(int s, int e, int key, int[] arr) {
-        int answer = 0;
-        int mid;
-
-        while (s <= e) {
-            mid = (s + e) / 2;
-
-            if (arr[mid] < key) {
-                s = mid + 1;
-            } else if (arr[mid] > key) {
-                e = mid - 1;
-            } else {
-                answer = 1;
-                break;
-            }
-        }
-        return answer;
-    }
-
-
     public static void main(String[] args) throws  IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        int[] cards = new int[N];
+        int[] cards = new int[20000010];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            cards[i] = Integer.parseInt(st.nextToken());
+            cards[Integer.parseInt(st.nextToken()) + 10000000]++;
         }
 
         int M = Integer.parseInt(br.readLine());
-        int[] nums = new int[M];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(cards);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            sb.append(binarySearch(0, N - 1, nums[i], cards)+" ");
+            if (cards[Integer.parseInt(st.nextToken()) + 10000000] > 0) {
+                sb.append(1 + " ");
+            } else {
+                sb.append(0 + " ");
+            }
         }
 
         bw.write(String.valueOf(sb));
