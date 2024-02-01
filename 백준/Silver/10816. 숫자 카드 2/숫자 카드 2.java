fe39ae9d215f833b281f0e8277f543bd9fd73ffd
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-01 19:11:08 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1612 ms, Memory: 140784 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,70 +1,78 @@
-
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
 
-    static int lowerBound(int[] arr, int key){
-        int left=0, right=arr.length-1;
-        int leftIdx = arr.length;
+    static int getLeftIdx(int s, int e, int key, int[] arr) {
+        int ans = -1;
+        int mid;
+        while (s <= e) {
+            mid = (s + e) / 2;
 
-        while(left<=right){
-            int mid = (left+right)/2;
-
-            if(arr[mid] < key) left = mid+1;
-            else{
-                right = mid-1;
-                leftIdx = mid;
+            if (arr[mid] == key) {
+                ans = mid;
+                e = mid - 1;
+            } else if (arr[mid] < key) {
+                s = mid + 1;
+            } else {
+                e = mid -1;
             }
         }
-        return leftIdx;
+        return ans;
     }
 
-    static int upperBound(int[] arr, int key){
-        int left=0, right=arr.length-1;
-        int rightIdx = arr.length;
+    static int getRightIdx(int s, int e, int key, int[] arr) {
+        int ans = -1;
+        int mid;
+        while (s <= e) {
+            mid = (s + e) / 2;
 
-        while(left<=right){
-            int mid = (left+right)/2;
-
-            if(arr[mid] <= key) left = mid+1;
-            else{
-                right = mid-1;
-                rightIdx = mid;
+            if (arr[mid] == key) {
+                ans = mid;
+                s = mid + 1;
+            } else if (arr[mid] < key) {
+                s = mid + 1;
+            } else {
+                e = mid -1;
             }
         }
-        return rightIdx;
+        return ans;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws  IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        int[] lists = new int[N];
+        int[] cards = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            lists[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(lists);
 
         int M = Integer.parseInt(br.readLine());
-        int[] targets = new int[M];
+        int[] nums = new int[M];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<M; i++){
-            targets[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < M; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
+        Arrays.sort(cards);
+        int r, l;
+        for (int i = 0; i < M; i++) {
+            r = getRightIdx(0, N - 1, nums[i], cards);
+            l = getLeftIdx(0, N - 1, nums[i], cards);
 
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<M; i++){
-            int leftIdx = lowerBound(lists, targets[i]);
-            int rightIdx = upperBound(lists, targets[i]);
-            sb.append(rightIdx-leftIdx).append(' ');
+            if( r < 0 || l < 0) sb.append(0 + " ");
+            else sb.append(r - l + 1 + " ");
         }
 
-        System.out.println(sb);
-
-;    }
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
