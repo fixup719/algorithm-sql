334ac2d7105750531a90f17c54e7c448796db5c1
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-01 17:55:28 +0900

    [Silver V] Title: 숫자 카드, Time: 1360 ms, Memory: 122924 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,42 +1,58 @@
-
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 
 public class Main {
 
-    static int binarySearch(int key, int start, int end, int[] arr){
-        int mid = (start+end)/2;
+    static int binarySearch(int s, int e, int key, int[] arr) {
+        int answer = 0;
+        int mid;
 
-        if(start > end ) return 0;
+        while (s <= e) {
+            mid = (s + e) / 2;
 
-        if(arr[mid] == key) return 1;
-        else if(arr[mid] < key) return binarySearch(key, mid+1, end, arr);
-        else return binarySearch(key, start, mid-1, arr);
+            if (arr[mid] < key) {
+                s = mid + 1;
+            } else if (arr[mid] > key) {
+                e = mid - 1;
+            } else {
+                answer = 1;
+                break;
+            }
+        }
+        return answer;
     }
 
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws  IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-
-        st = new StringTokenizer(br.readLine());
         int[] cards = new int[N];
-        for(int i=0; i<N; i++){
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
             cards[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(cards);
 
         int M = Integer.parseInt(br.readLine());
-        
+        int[] nums = new int[M];
         st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        while(M-->0){
-            int num = Integer.parseInt(st.nextToken());
-            sb.append(binarySearch(num, 0, cards.length-1, cards)+" ");
+        for (int i = 0; i < M; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(sb);
 
+        Arrays.sort(cards);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            sb.append(binarySearch(0, N - 1, nums[i], cards)+" ");
+        }
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
     }
-
 }
