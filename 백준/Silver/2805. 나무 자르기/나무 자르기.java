e4653fa719076766c7d5c877433cf30858f7731a
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-03 23:52:05 +0900

    [Silver II] Title: 나무 자르기, Time: 536 ms, Memory: 119392 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,49 +1,56 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
+    static int N, M;
+    static int[] arr;
+    static int answer = 0;
 
-    static int parametricSearch(int[] arr, int target){
-        int left=0, right=arr[arr.length-1];
+    static void binarySearch(int s, int e) {
 
-        while(left<=right){
-            int mid = (left+right)/2;
+        int mid;
+        long sum;
+        while (s <= e) {
+            mid = (s + e) / 2;
 
-            long sum=0;
-            for(int i=0; i<arr.length; i++){
-                if(arr[i] > mid){
-                    sum += (arr[i]-mid);
-                }
+            sum = 0;
+            for (int i = 0; i < N; i++) {
+                if(arr[i] >= mid) sum += arr[i] - mid;
             }
 
-            if(sum < target){
-                // 높이를 더 낮게 지정해야함
-                right = mid-1;
-            }else if(sum > target){
-                // 높이를 더 높게 지정해야함
-                left = mid+1;
-            }else return mid;
+            if (sum < M) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+                answer = Math.max(answer, mid);
+            }
         }
-        return (left+right)/2;
+
     }
 
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws  IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int[] trees = new int[N];
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            trees[i] = Integer.parseInt(st.nextToken());
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
         }
-        Arrays.sort(trees);
 
-        System.out.println(parametricSearch(trees, M));
-;    }
+        binarySearch(0, max);
+
+
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
