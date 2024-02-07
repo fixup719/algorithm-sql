03dc0d05ab4ea3b7120733c8e199dbb0fe7f7a9c
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-07 20:55:19 +0900

    [Gold V] Title: 두 용액, Time: 444 ms, Memory: 28668 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,47 +1,71 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
-    static int minLeft = 0;
-    static int minRight = 0;
+    static int answer = Integer.MAX_VALUE;
+    static int[] ansValue = new int[2];
 
-    static void binarySearch(int[] arr){
-        int left = 0, right = arr.length-1;
-        int mixValue = Integer.MAX_VALUE;
+    static void binarySearch(int s, int e, int key, int[] arr) {
+        int mid;
+        while (s <= e) {
+            mid = (s + e) / 2;
 
-        while(left < right){
-            int mixTemp = arr[left]+arr[right];
-
-            if(mixValue > Math.abs(mixTemp)){
-                mixValue = Math.abs(mixTemp);
-                minLeft = left;
-                minRight = right;
-
-                if(mixTemp == 0) break;
+            if (key < 0) {
+                // key가 음수인 경우
+                if (arr[mid] < 0) {
+                    // 비교하려는 후보로 음수가 선택 됐을 때, 해당 후보 왼쪽은 무조건 제거
+                    s = mid + 1;
+                } else {
+                    // 비교하려는 후보로 양수가 선택 됐을 때,
+                    // 선택된 key값의 절대값보다 해당 후보가 클 경우 오른쪽 후보들은 제거
+                    // 선택된 key값의 절대값보다 해당 후보가 작을 경우 왼쪽 후보들을 제거
+                    if(Math.abs(key) < arr[mid]) e = mid - 1;
+                    else s = mid + 1;
+                }
+            } else {
+                // key가 양수인 경우
+                if (arr[mid] > 0) {
+                    // 비교하려는 후보로 양수가 선택 됐을 때, 해당 후보 오른쪽은 무조건 제거
+                    e = mid -1;
+                } else {
+                    // 비교하려는 후보로 음수가 선택 됐을 때,
+                    // 선택된 key값의 절대값보다 해당 후보의 절대값이 클 경우 왼쪽 후보들은 제거
+                    // 선택된 key값의 절대값보다 해당 후보의 절대값 작을 경우 오른쪽 후보들을 제거
+                    if(key > Math.abs(arr[mid])) e = mid - 1;
+                    else s = mid + 1;
+                }
             }
 
-            if(mixTemp < 0) left++;
-            else right--;
+            if (key != arr[mid] && answer > Math.abs(arr[mid] + key)) {
+                answer = Math.abs(arr[mid] + key);
+                ansValue[0] = key;
+                ansValue[1] = arr[mid];
+            }
 
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws  IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-
-        int[] arr = new int[N];
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+        int[] values = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            values[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
-        binarySearch(arr);
-        System.out.println(arr[minLeft]+" "+arr[minRight]);
+        Arrays.sort(values);
+        for (int i = 0; i < N; i++) {
+            binarySearch(0, N-1, values[i], values);
+        }
 
-;    }
+        Arrays.sort(ansValue);
+        bw.write(String.valueOf(ansValue[0] + " " + ansValue[1]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
