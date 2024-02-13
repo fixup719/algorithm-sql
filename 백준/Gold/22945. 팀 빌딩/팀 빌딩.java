26c2556b18dbda2a931c6fda255ef287e1d47ea9
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-13 12:29:34 +0900

    [Gold IV] Title: 팀 빌딩, Time: 296 ms, Memory: 24336 KB -BaekjoonHub

--- 백준/Gold/22945. 팀 빌딩/팀 빌딩.java
+++ 백준/Gold/22945. 팀 빌딩/팀 빌딩.java
@@ -1,31 +1,31 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws  IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+        int N  = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int s = 0, e = N - 1, ans = 0;
-        while (s < e) {
+        int s = 0, e = N - 1;
+        int answer = 0;
+        while (s <= e) {
+            answer = Math.max(answer, (e - s - 1) * Math.min(arr[s], arr[e]));
 
-            ans = Math.max(ans, (e - s - 1) * Math.min(arr[s], arr[e]));
-            if (arr[s] > arr[e]) {
-                e--;
-            } else {
-                s++;
-            }
+            if(arr[s] < arr[e]) s++;
+            else e--;
         }
 
-        bw.write(String.valueOf(ans));
+        bw.write(String.valueOf(answer));
         bw.flush();
         bw.close();
         br.close();
