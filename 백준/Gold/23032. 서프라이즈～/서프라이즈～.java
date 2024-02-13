8c66d5041a0f7df12261ed71294166bfca0dab75
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-13 15:41:53 +0900

    [Gold III] Title: 서프라이즈~, Time: 240 ms, Memory: 14700 KB -BaekjoonHub

--- 백준/Gold/23032. 서프라이즈～/서프라이즈～.java
+++ 백준/Gold/23032. 서프라이즈～/서프라이즈～.java
@@ -4,62 +4,55 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static int[] grams;
-    static int answer = Integer.MAX_VALUE;
-    static int ansGram = 0;
+    static int[] arr;
+    static int ansDiff = Integer.MAX_VALUE;
+    static int ansSum;
 
-    static int binarySearch(int s, int e, int[] arr) {
-
+    static void binarySearch(int s, int e) {
+        int mid, g1, g2, diff;
         int start = s, end = e;
-        int mid, group1, group2, diff;
         while (s <= e) {
             mid = (s + e) / 2;
 
-            group1 = grams[mid] - grams[start - 1];
-            group2 = grams[end] - grams[mid];
+            g1 = arr[mid] - arr[start - 1];
+            g2 = arr[end] - arr[mid];
 
-            if(group1 > group2){
+            diff = Math.abs(g1 - g2);
+
+            if(ansDiff > diff){
+                ansDiff = diff;
+                ansSum = g1 + g2;
+            } else if (ansDiff == diff) {
+                if(ansSum < g1 + g2) ansSum = g1 + g2;
+            }
+
+            if (g1 > g2) {
                 e = mid - 1;
-            } else{
+            } else {
                 s = mid + 1;
             }
-
-            diff = Math.abs(group1 - group2);
-
-            if (answer > diff) {
-                answer = diff;
-                ansGram = group1 + group2;
-            } else if (answer == diff) {
-                if (ansGram < group1 + group2) {
-                    ansGram = group1 + group2;
-                }
-            }
         }
-
-        return answer;
     }
 
     public static void main(String[] args) throws  IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(br.readLine());
+        N  = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        grams = new int[N + 1];
+        arr = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            grams[i] = grams[i - 1] + Integer.parseInt(st.nextToken());
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
         }
 
-        // 대상 그룹 시작 인덱스 선택
         for (int i = 1; i <= N - 1; i++) {
-            // 대상 그룹 끝 인덱스 선택(2명이상 N명이하)
-            for (int j = i+1; j <= N; j++) {
-                binarySearch(i, j, grams);
+            for (int j = i + 1; j <= N; j++) {
+                binarySearch(i, j);
             }
         }
 
-        bw.write(String.valueOf(ansGram));
+        bw.write(String.valueOf(ansSum));
         bw.flush();
         bw.close();
         br.close();
