34de175da2af0b73ad58cac626317e050ca66f2c
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-18 23:43:09 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 120 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(X * X * X));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
