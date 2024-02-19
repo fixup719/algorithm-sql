1465af581de4dc59c65ebaa3c77ccf1e5c318b46
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-19 10:04:04 +0900

    [Silver II] Title: 부분수열의 합, Time: 212 ms, Memory: 14780 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,44 +1,55 @@
-
-
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, S;
-    static int[] nums;
-    static int cnt = 0;
+    static int[] arr;
+    static int[] selected;
+    static int size, cnt;
 
-    static void comb(int depth, int start, int sum, int m){
-        if(depth == m){
-            if(sum == S) cnt++;
+    static void recur(int cur, int start) {
+        if (cur == size) {
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                sum += arr[selected[i]];
+            }
+            if (sum == S) {
+                cnt++;
+            }
             return;
         }
 
-        for(int i=start; i<N; i++){
-            sum += nums[i];
-            comb(depth+1, i+1, sum, m);
-            sum -= nums[i];
+        for (int i = start; i <= N; i++) {
+            selected[cur] = i;
+            recur(cur + 1, i + 1);
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
 
-        nums = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int m=1; m<=N; m++){
-            comb(0,0,0,m);
+        selected = new int[N];
+        for (int i = 1; i <= N; i++) {
+            // 크기가 1~N개인 경우
+            size = i;
+            recur(0, 1);
         }
 
-        System.out.println(cnt);
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
