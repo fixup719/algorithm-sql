c6068f1a45fca641881511b7677b90d13609f780
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-19 16:57:10 +0900

    [Gold V] Title: 치킨 배달, Time: 236 ms, Memory: 16932 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,76 +1,84 @@
-
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M; // 지도크기, 남겨둘 치킨집 개수
-    static int[][] city; // 도시 맵 0빈칸, 1집, 2치킨집
-    static ArrayList<int[]> houses;
-    static ArrayList<int[]> chickens;
-    static int[][] selected;
+    static int N, M;
+    static ArrayList<Point> chickens = new ArrayList<>();
+    static ArrayList<Point> houses = new ArrayList<>();
+    static int chickenCnt, houseCnt;
+    static int[] selected;
     static int answer = Integer.MAX_VALUE;
 
-    static void comb(int depth, int start){
+    static class Point {
+        int row;
+        int col;
 
-        if(depth == M){
-            // 도시의 치킨 거리 구하기
-            answer = Math.min(answer, countDist());
-            return;
-        }
-
-        for(int i=start; i<chickens.size(); i++){
-            selected[depth][0] = chickens.get(i)[0];
-            selected[depth][1] = chickens.get(i)[1];
-            comb(depth+1, i+1);
+        Point(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
     }
 
-    static int countDist(){
-        int sum = 0;
-        for(int i=0; i<houses.size(); i++){
-            int min = 100;
-            int hrow = houses.get(i)[0];
-            int hcol = houses.get(i)[1];
-            for(int j=0; j<M; j++){
-                int crow = selected[j][0];
-                int ccol = selected[j][1];
-                min = Math.min(min, Math.abs(hrow-crow)+Math.abs(hcol-ccol));
+    static void recur(int cur, int start) {
+        if (cur == M) {
+
+            int sum = 0, dist, hr, hc, kr, kc;
+            for (int i = 0; i < houseCnt; i++) {
+                dist = 1000;
+                hr = houses.get(i).row;
+                hc = houses.get(i).col;
+                for (int j = 0; j < M; j++) {
+                    kr = chickens.get(selected[j]).row;
+                    kc = chickens.get(selected[j]).col;
+                    dist = Math.min(dist, Math.abs(hr - kr) + Math.abs(hc - kc));
+                }
+                sum += dist;
             }
-            sum += min;
+
+            answer = Math.min(answer, sum);
+
+            return;
         }
-        return sum;
+
+        for (int i = start; i < chickenCnt; i++) {
+            selected[cur] = i;
+            recur(cur + 1, i + 1);
+        }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        city = new int[N][N];
-        houses = new ArrayList<>();
-        chickens = new ArrayList<>();
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
-                int input = Integer.parseInt(st.nextToken());
-                city[i][j] = input;
+            int input;
+            for (int j = 0; j < N; j++) {
+                input = Integer.parseInt(st.nextToken());
 
-                // 집, 치킨집 좌표 정보 입력
-                if(input == 1) {
-                    houses.add(new int[] {i, j});
-                }else if(input == 2) {
-                    chickens.add(new int[] {i, j});
+                if (input == 2) {
+                    chickens.add(new Point(i, j));
+                } else if (input == 1) {
+                    houses.add(new Point(i, j));
                 }
             }
         }
 
-        selected = new int[M][2];
-        comb(0,0);
+        chickenCnt = chickens.size();
+        houseCnt = houses.size();
+        selected = new int[M];
 
-        System.out.println(answer);
+        recur(0, 0);
+
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+        br.close();
     }
-
 }
