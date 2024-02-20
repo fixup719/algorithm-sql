36d251b7a6f6a936d83e67d680997c05bcf6fdcf
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-21 00:10:40 +0900

    [Gold IV] Title: N-Queen, Time: 5312 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,62 +1,46 @@
-
 import java.io.*;
-import java.util.*;
 
 public class Main {
     static int N;
-    static int[][] board;
-    static int cnt;
-    static int[] queens;
+    static int[] selected;
+    static int answer;
 
-    /*
-       일단 한번 방문한 row, col은 재방문 X
-       대각선의 경우
-       - 정답군의 좌표와 후보 좌표간 차이를 이용해서
-            - row, col각각의 차이값이 같거나
-            - row, col 각각의 차이값을 서로 더할 때 0인 경우
-            => 위 케이스를 필터링
-    * */
+    static boolean check(int cur) {
+        for (int i = 0; i < cur; i++) {
+            if (selected[cur] == selected[i]
+                    || Math.abs(cur - i) == Math.abs(selected[cur] - selected[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-    static void dfs(int depth){
-
-        if(depth == N){
-            cnt++;
+    static void recur(int cur) {
+        if (cur == N) {
+            answer++;
             return;
         }
 
-       for(int col = 0; col<N; col++){
-            queens[depth] = col;
-
-            if(isPossible(depth)){
-                dfs(depth+1);
+        for (int i = 0; i < N; i++) {
+            selected[cur] = i;
+            if (check(cur)) {
+                recur(cur + 1);
             }
         }
     }
 
-    static boolean isPossible(int depth){
-        boolean isPossible = true;
-
-        for(int i=0; i<depth; i++){
-
-            if(queens[depth] == queens[i]) return false; // 같은 열 필터링
-
-            if(Math.abs(depth-i) == Math.abs(queens[depth]-queens[i])) return false; // 대각선 존재 필터링
-
-        }
-
-
-        return isPossible;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         N = Integer.parseInt(br.readLine());
 
-        board = new int[N][N];
-        queens = new int[N];
+        selected = new int[N];
+        recur(0);
 
-        dfs(0);
-        System.out.println(cnt);
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+        br.close();
     }
-
 }
