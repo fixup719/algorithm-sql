009799f25ec08493a7db10c5402c1daac6a288bd
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-21 19:07:40 +0900

    [Silver II] Title: N과 M (9), Time: 664 ms, Memory: 48008 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,63 +1,56 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, M;
-	static int[] nums;
-	static int[] ans;
-	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void perm(int depth) {
-		
-		if(depth == M) {
-			for(int i=0; i<M; i++) {
-				sb.append(ans[i] + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		int before=0;
-		for(int i=0 ; i<N; i++) {
-			if(visited[i]) continue;
-			
-			if(before != nums[i]) {
-				visited[i] = true;
-				ans[depth] = nums[i];
-				before = nums[i];
-				perm(depth+1);
-				visited[i] = false;
-			}
-			
-		}
-	}
-	
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		nums = new int[N];
-		for(int i=0; i<N; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(nums);
-		
-		ans = new int[M];
-		visited = new boolean[N];
-		perm(0);
-		System.out.println(sb);
-	}
+    static int N, M;
+    static int[] nums;
+    static int[] answer;
+    static boolean[] visited;
+    static Set<String> set = new LinkedHashSet<>();
+    static StringBuilder sb = new StringBuilder();
 
+    public static void recur(int cur) {
+        if (cur == M) {
+            sb.setLength(0);
+            for (int i = 0; i < M; i++) {
+                sb.append(answer[i] + " ");
+            }
+            set.add(String.valueOf(sb));
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            answer[cur] = nums[i];
+            recur(cur + 1);
+            visited[i] = false;
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+        answer = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        recur(0);
+
+        String[] ans = set.toArray(new String[set.size()]);
+        for (String e : ans) {
+            System.out.println(e);
+        }
+
+        br.close();
+    }
 }
