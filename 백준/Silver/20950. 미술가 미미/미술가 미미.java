8c729850f278609aa4ffe9fdd03e9877387cfe37
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-23 18:23:00 +0900

    [Silver II] Title: 미술가 미미, Time: 176 ms, Memory: 12440 KB -BaekjoonHub

--- 백준/Silver/20950. 미술가 미미/미술가 미미.java
+++ 백준/Silver/20950. 미술가 미미/미술가 미미.java
@@ -2,33 +2,35 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[][] colors;
-    static int[] gomduri;
-    static int size;
-    static int[] selected;
-    static int answer = Integer.MAX_VALUE;
+    static int N; // 물감 개수
+    static int[][] colors; // 물감들의 RGB 정보
+    static int[] gomduri; // 곰두리 색의 RGB 정보
+    static int answer = Integer.MAX_VALUE; // 차이값
 
-    static void recur(int cur, int start) {
-        if (cur == size) {
+    static void recur(int cur, int cnt, int r, int g, int b) {
 
-            int R = 0, G = 0, B = 0;
-            for (int i = 0; i < size; i++) {
-                R += colors[selected[i]][0];
-                G += colors[selected[i]][1];
-                B += colors[selected[i]][2];
+        if (cnt > 7) return; // 개수가 7개를 넘어가는 순간 pass
+
+        if (cur == N) {
+
+            // 선택한 개수가 1개 이하 또는 8개 이상일 경우 pass
+            if (cnt < 2) {
+                return;
             }
 
-            answer = Math.min(answer, Math.abs(gomduri[0] - R / size)
-                    + Math.abs(gomduri[1] - G / size) + Math.abs(gomduri[2] - B / size));
+            // 곰두리색과의 차이값 갱신
+            answer = Math.min(answer
+                    , Math.abs(gomduri[0] - r / cnt) + Math.abs(gomduri[1] - g / cnt) + Math.abs(gomduri[2] - b / cnt));
 
             return;
         }
 
-        for (int i = start; i < N; i++) {
-            selected[cur] = i;
-            recur(cur + 1, i + 1);
-        }
+        // 색상을 선택한 경우
+        recur(cur + 1, cnt + 1
+                , r + colors[cur][0], g + colors[cur][1], b + colors[cur][2]);
+
+        // 색상을 선택하지 않은 경우
+        recur(cur + 1, cnt, r, g, b);
     }
 
     public static void main(String[] args) throws IOException {
@@ -36,7 +38,9 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());    // 물감 개수 입력 받기
+
+        // 물감 RGB 정보 입력 받기
         colors = new int[N][3];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -45,20 +49,14 @@ public class Main {
             colors[i][2] = Integer.parseInt(st.nextToken());
         }
 
+        // 곰두리 색 RGB 정보 입력 받기
         gomduri = new int[3];
         st = new StringTokenizer(br.readLine());
         gomduri[0] = Integer.parseInt(st.nextToken());
         gomduri[1] = Integer.parseInt(st.nextToken());
         gomduri[2] = Integer.parseInt(st.nextToken());
 
-        selected = new int[N];
-        for (int i = 2; i <= 7; i++) {
-            if(i > N) break;
-
-            size = i;
-
-            recur(0, 0);
-        }
+        recur(0, 0, 0, 0, 0);
 
         bw.write(String.valueOf(answer));
         bw.flush();
