8fdfbb83913b3cab11144e9e8b16c30f92435bd2
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-02-23 16:53:36 +0900

    [Gold IV] Title: 다이어트, Time: 96 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.java
+++ 백준/Gold/19942. 다이어트/다이어트.java
@@ -1,111 +1,94 @@
-
 import java.io.*;
-import java.util.*;
-
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N; // 식재료 개수
-    static int M = 1; // 몇 개의 재료를 선택할 지
-    static int[] minNutrients; // 최소 영양성분
-    static int[][] foods; // 식재료 정보 담을 배열
-    static int[] peek; // 선택한 식재료 번호 담을 배열
-    static int ansCost = Integer.MAX_VALUE; // 최소 비용
-    static List<String> ansList = new ArrayList<>(); // 최소 비용이 되는 재료집합 후보들
+    static int N;
+    static int[] standard;
+    static int[][] foods;
+    static int[] selected;
+    static ArrayList<String> ansFoods = new ArrayList<>();
+    static int answer = Integer.MAX_VALUE;
 
-    static boolean isAble(){
-        boolean check = true;
+    static void recur(int cur, int cnt) {
+        if (cur == N + 1) {
 
-        // 재료 영양성분 합계 입력할 배열 선언
-        int[] sumNutrients = new int[4];
-        for(int i=0; i<M; i++){
-            for(int j=0; j<4; j++){
-                sumNutrients[j] += foods[peek[i]][j];
+            int p = 0, f = 0, s = 0, v = 0, c = 0;
+            for (int i = 0; i < cnt; i++) {
+                p += foods[selected[i]][0];
+                f += foods[selected[i]][1];
+                s += foods[selected[i]][2];
+                v += foods[selected[i]][3];
+                c += foods[selected[i]][4];
             }
-        }
 
-        // 최소 기준 충족하는지 체크
-        for(int i=0; i<4; i++){
-            if(sumNutrients[i] < minNutrients[i]){
-                check = false;
-                break;
+            if (p < standard[0] || f < standard[1] || s < standard[2] || v < standard[3]) {
+                return;
             }
-        }
-        return check;
-    }
 
-    static void comb(int depth, int start){
-        if(depth == M){
-            if(isAble()){
+            if (answer >= c) {
 
-                // 비용 합계 구하기
-                int cost = 0;
-                for(int i=0; i<M; i++){
-                    cost += foods[peek[i]][4];
+                if (answer > c) {
+                    answer = c;
+                    ansFoods.clear();
                 }
 
-
-                if(ansCost >= cost){
-                    if(ansCost>cost){
-                        // 최소 비용 갱신하는 경우 => 기존 후보군들 제거
-                        ansList.clear();
-                    }
-                    // 후보군 추가
-                    ansCost = cost;
-                    String ansPeek = "";
-                    for (int el : peek) {
-                        ansPeek += (el+1)+" ";
-                    }
-
-                    ansList.add(ansPeek);
+                String str = "" ;
+                for (int i = 0; i < cnt; i++) {
+                    str += selected[i] + " ";
                 }
+                ansFoods.add(str);
             }
+
 
             return;
         }
 
-        for(int i=start; i<N; i++){
-            peek[depth] = i;
-            comb(depth+1, i+1);
-        }
+        selected[cnt] = cur;
+        recur(cur + 1, cnt + 1);
+
+        selected[cnt] = 0;
+        recur(cur + 1, cnt);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
 
-        minNutrients = new int[4];
-        foods = new int[N][5];
-
-        // 최소 영양 성분 입력
+        standard = new int[4];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<4; i++){
-            minNutrients[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < 4; i++) {
+            standard[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 식재료 정보 입력
-        for(int i=0; i<N; i++){
+        foods = new int[N + 1][5];
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<5; j++){
+            for (int j = 0; j < 5; j++) {
                 foods[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 1부터 N개까지 선택 NC1 ~ NCN
-        while(M<=N){
-            peek = new int[M];
-            comb(0,0);
-            M++;
+        selected = new int[N];
+
+        recur(1, 0);
+
+        if (answer == Integer.MAX_VALUE) {
+            bw.write("-1");
+        } else {
+            bw.write(answer + "\n");
+
+            Collections.sort(ansFoods);
+            bw.write(String.valueOf(ansFoods.get(0)));
         }
 
-        if(ansList.size()==0){
-            System.out.println(-1);
-        }else{
-            System.out.println(ansCost);
-            Collections.sort(ansList);
-            System.out.println(ansList.get(0));
-        }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
