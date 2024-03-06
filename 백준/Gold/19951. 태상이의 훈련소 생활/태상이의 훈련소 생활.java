c29355d52a77eb07bdede12e011787719366396f
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-03-06 18:39:39 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 608 ms, Memory: 77528 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -1,45 +1,49 @@
-
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
+        int[] arr = new int[N + 1];
         st = new StringTokenizer(br.readLine());
-        int[] blocks = new int[N];
-        for(int i=0; i<N; i++){
-            blocks[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] deltas = new int[N+1];
-        int[] acc = new int[N];
-        while(M-->0){
+        int a, b, k;
+        int[] imos = new int[N + 2];
+        while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken())-1;
-            int end = Integer.parseInt(st.nextToken())-1;
-            int size = Integer.parseInt(st.nextToken());
 
-            deltas[start] += size;
-            deltas[end+1] += -size;
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+
+            imos[a] += k;
+            imos[b + 1] += -(k);
         }
 
-        acc[0] = deltas[0];
-        for(int i=1; i<N; i++){
-            acc[i] = acc[i-1]+deltas[i];
+        for (int i = 1; i <= N; i++) {
+            imos[i] += imos[i - 1];
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<N; i++) {
-            sb.append(blocks[i]+acc[i]+" ");
+        for (int i = 1; i <= N; i++) {
+            sb.append((imos[i] + arr[i]) + " ");
         }
-        System.out.println(sb);
 
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
