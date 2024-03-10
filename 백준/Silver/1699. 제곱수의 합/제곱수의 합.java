caf9904ce886fd44929436d88456c71c5a33f551
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-03-10 15:55:13 +0900

    [Silver II] Title: 제곱수의 합, Time: 156 ms, Memory: 12212 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    static int N;
+    static int[] dp;
+/*
+     1. 백트랙킹으로 구현하기
+    static void recur(int remain, int cnt) {
+
+        if (remain == 0) {
+            answer = Math.min(answer, cnt);
+            return;
+        }
+
+        for (int i = (int)Math.sqrt(remain); i >= 1; i--) {
+            remain -= i*i;
+            recur(remain, cnt + 1);
+
+            remain += i * i;
+        }
+
+    }
+ */
+
+    // 2. 함수  + 메모이제이션 적용하기 => 현재 남은 숫자에서 제곱수의 합으로 나타낼 수 있는 가장 최소 항을 반환
+    static int recur( int remain) {
+
+//        if (remain < 0) return 1 << 30;
+
+        if (remain == 0) return 0;
+
+        if (dp[remain] != 1<<30) return dp[remain];
+
+        for (int i = (int)Math.sqrt(remain); i >= 1; i--) {
+
+            dp[remain] = Math.min(recur( remain - i * i) + 1, dp[remain]);
+
+        }
+
+        return dp[remain];
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+
+        dp = new int[N + 50];
+        Arrays.fill(dp, 1<<30);
+
+        bw.write(String.valueOf(recur(N)));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
