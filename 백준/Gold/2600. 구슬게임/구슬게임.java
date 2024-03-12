1bae98af572373d79ae089d025a0f7d624116d10
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-03-13 08:21:26 +0900

    [Gold IV] Title: 구슬게임, Time: 112 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Gold/2600. 구슬게임/구슬게임.java
+++ 백준/Gold/2600. 구슬게임/구슬게임.java
@@ -1,50 +1,59 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int[] balls;
+    static int[] takeOut;
+
+    static int[][] marbles;
     static int[][] dp;
-    static int[][] container;
+
+    static int recur(int box1, int box2) {
+        // A => who :true / B => who : false
+        if (dp[box1][box2] != -1) return dp[box1][box2];
+
+        int ret = 2;
+        for (int i = 0; i < 3; i++) {
+            if (box1 - takeOut[i] >= 0 && recur(box1 - takeOut[i], box2) == 2) ret = 1;
+        }
+        for (int i = 0; i < 3; i++) {
+            if (box2 - takeOut[i] >= 0 && recur(box1, box2 - takeOut[i]) == 2) ret = 1;
+        }
+        dp[box1][box2] = ret;
+        return dp[box1][box2];
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        balls = new int[3];
-        dp = new int[510][510];
-        for(int i=0; i<3; i++){
-            balls[i]=Integer.parseInt(st.nextToken());
+        takeOut = new int[3];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 3; i++) {
+            takeOut[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(balls);
-        container = new int[5][2];
-        int flag;
+
+        marbles = new int[5][2];
         for (int i = 0; i < 5; i++) {
-            st=new StringTokenizer(br.readLine());
-            container[i][0]=Integer.parseInt(st.nextToken());
-            container[i][1]=Integer.parseInt(st.nextToken());
-            flag = dp(container[i][0],container[i][1]);
-            System.out.println(flag==1?"A":"B");
-        }
-    }
-
-    // 2는 B가 승리하는 경우 1은 A가 승리하는 경우 0은 아직 방문이 안된 dp
-    private static int dp(int k1, int k2) {
-        if(k1<balls[0] && k2 <balls[0]) return 2;
-
-        if(dp[k1][k2]!=0){
-            return dp[k1][k2];
+            st = new StringTokenizer(br.readLine());
+            marbles[i][0] = Integer.parseInt(st.nextToken());
+            marbles[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        int result =2;
-
-        for(int i=0; i<3; i++){
-            if(k1>=balls[i])
-                if(dp(k1-balls[i],k2)==2)result=1;
-            if(k2>=balls[i])
-                if(dp(k1,k2-balls[i])==2)result=1;
+        dp = new int[510][510];
+        for (int i = 0; i < 510; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        dp[k1][k2]=result;
-        return result;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 5; i++) {
+            if (recur(marbles[i][0], marbles[i][1]) == 1) sb.append('A' + "\n");
+            else sb.append('B' + "\n");
+        }
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
