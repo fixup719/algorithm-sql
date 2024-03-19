8abc2f3bd84f7b466f12fa71f3deca73dbb8dcd9
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-03-19 21:36:08 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 55100 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,24 +1,7 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
-    static int[][] arr;
-    static int[][] dp;
-    static int recur(int cur, int weight) {
-
-        if (weight > K) return -1000000000;
-
-        if (cur == N) return 0;
-
-        if (dp[cur][weight] != -1) return dp[cur][weight];
-
-        int ret = Math.max(recur(cur + 1, weight + arr[cur][0]) + arr[cur][1], recur(cur + 1, weight));
-        dp[cur][weight] = ret;
-
-        return dp[cur][weight];
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,22 +9,32 @@ public class Main {
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        arr = new int[N][2];
+        int[][] arr = new int[N][2];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        dp = new int[N][100010];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[N + 10][K + 10];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+
+                dp[i][j] = dp[i - 1][j];
+
+                if (j < arr[i - 1][0]) {
+                    continue;
+                }
+
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - arr[i - 1][0]] + arr[i - 1][1]);
+            }
         }
 
-        bw.write(String.valueOf(recur(0, 0)));
+        bw.write(String.valueOf(dp[N][K]));
         bw.flush();
         bw.close();
         br.close();
