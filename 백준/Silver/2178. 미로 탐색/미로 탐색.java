1c6f4657c38f6746dea421fdab2116254b95fe30
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-03-29 15:48:16 +0900

    [Silver I] Title: 미로 탐색, Time: 96 ms, Memory: 12388 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,66 +1,72 @@
-
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M; // 목적지(N,M)
+    static int N, M;
     static int[][] map;
     static boolean[][] visited;
-    static int[] delR = {-1,1,0,0};
-    static int[] delC = {0,0,-1,1};
-    static int cnt = 0;
+    static int[] delR = {-1, 1, 0, 0};
+    static int[] delC = {0, 0, -1, 1};
+    static int dist;
 
-    static void bfs(int startR, int startC){
+    static void bfs(int row, int col) {
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{startR,startC});
-        visited[startR][startC] = true;
-        cnt++;
-        wh : while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                int tRow = q.peek()[0];
-                int tCol = q.poll()[1];
+        q.offer(new int[]{row, col});
+        visited[row][col] = true;
 
-                for(int d=0; d<4; d++){
-                    int mRow = tRow + delR[d];
-                    int mCol = tCol + delC[d];
+        int cRow, cCol, mRow, mCol, size;
+        while (!q.isEmpty()) {
 
-                    if(mRow<0 || mCol<0 || N<=mRow || M<=mCol) continue;
+            size = q.size();
 
-                    if(!visited[mRow][mCol] && map[mRow][mCol]==1){
-                        q.offer(new int[]{mRow, mCol});
-                        visited[mRow][mCol] = true;
-                    }
+            for (int i = 0; i < size; i++) {
+                cRow = q.peek()[0];
+                cCol = q.poll()[1];
 
-                    if(mRow == N-1 && mCol == M-1){
-                        cnt++;
-                        break wh;
-                    }
+                if (cRow == N - 1 && cCol == M - 1) return;
+
+                for (int dir = 0; dir < 4; dir++) {
+                    mRow = cRow + delR[dir];
+                    mCol = cCol + delC[dir];
+
+                    if (mRow < 0 || mCol < 0 || N <= mRow || M <= mCol
+                            || visited[mRow][mCol] || map[mRow][mCol] == 0) continue;
+
+                    q.offer(new int[]{mRow, mCol});
+                    visited[mRow][mCol] = true;
                 }
             }
-            cnt++;
+
+            dist++;
         }
+
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N][M];
-        for(int i=0; i<N; i++){
-            String[] input = br.readLine().split("");
-            for(int j=0; j<M; j++){
-                map[i][j] = Integer.parseInt(input[j]);
+        String input;
+        for (int i = 0; i < N; i++) {
+            input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = input.charAt(j) - '0';
             }
         }
 
         visited = new boolean[N][M];
-        bfs(0,0);
 
-        System.out.println(cnt);
+        bfs(0, 0);
+
+        bw.write(String.valueOf(dist + 1));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
