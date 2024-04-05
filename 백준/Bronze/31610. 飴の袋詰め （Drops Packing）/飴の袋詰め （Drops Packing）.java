4ea8295588f1414c859100de51cc1b44dcfbecab
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-04-05 18:04:21 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 96 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws  IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(A * B + C));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
