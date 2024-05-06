4166716eb2d4f1c8bc97379c829bdc4965be4c96
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-05-06 16:41:21 +0900

    [Gold II] Title: 구간 자르기, Time: 116 ms, Memory: 16284 KB -BaekjoonHub

--- 백준/Gold/2283. 구간 자르기/구간 자르기.java
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.java
@@ -3,37 +3,23 @@ import java.util.*;
 
 public class Main {
     static int N, K;
-    static int[][] arr;
     static int[] imosPrefix;
-
-    // 구간 범위 안에 포함 된 구간 개수 반환
-    static int check(int s, int e) {
-        int cnt = 0;
-
-        for (int i = 0; i < N; i++) {
-            if (arr[i][0] <= e && s <= arr[i][1]) cnt++;
-        }
-
-        return cnt;
-    }
 
     // 구간 합이 K가 되는 경우 찾기
     static int[] twoPointer() {
         int[] answer = {0, 0};
-        int s = 1, e = 1;
-        int sum;
-        while (e < 1000010) {
-            sum = imosPrefix[e] - imosPrefix[s - 1] - check(s, e);
-
+        int s = 0, e = 0;
+        int sum = 0;
+        while (e < 1_000_010) {
             if (sum < K) {
-                // 구간 합을 늘려야 하므로
+                sum += imosPrefix[e];
                 e++;
             } else if (sum > K) {
-                // 구간 합을 줄여야 하므로
+                sum -= imosPrefix[s];
                 s++;
             } else {
-                answer[0] = s - 1;
-                answer[1] = e - 1;
+                answer[0] = s;
+                answer[1] = e;
                 break;
             }
         }
@@ -49,28 +35,19 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        arr = new int[N][2];
-
-        imosPrefix = new int[1000010];
+        imosPrefix = new int[1_000_010];
         
         int s, e;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            s = Integer.parseInt(st.nextToken()) + 1;
-            e = Integer.parseInt(st.nextToken()) + 1;
-
-            arr[i][0] = s;
-            arr[i][1] = e;
+            s = Integer.parseInt(st.nextToken());
+            e = Integer.parseInt(st.nextToken());
         
             imosPrefix[s]++;
-            imosPrefix[e + 1]--;
+            imosPrefix[e]--;
         }
 
-        for (int i = 1; i < 1000010; i++) {
-            imosPrefix[i] += imosPrefix[i - 1];
-        }
-
-        for (int i = 1; i < 1000010; i++) {
+        for (int i = 1; i < 1_000_010; i++) {
             imosPrefix[i] += imosPrefix[i - 1];
         }
 
