33c1ba24c677f5092d22c399c88800f80937523c
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-05-08 01:41:38 +0900

    [Gold III] Title: 소문난 칠공주, Time: 412 ms, Memory: 169196 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -15,8 +15,9 @@ public class Main {
     static void comb(int cur, int start, int yCnt) {
 
         if (cur == 7) {
+            // 임도연이 3명이하인 경우만 가능하므로
             if (yCnt < 4) {
-                // 인접한 좌석인지 체크하기
+                // 인접한 좌석이라면 answer증가
                 if (isNear()) answer++;
             }
             return;
@@ -25,6 +26,7 @@ public class Main {
         for (int i = start; i < 25; i++) {
             selected[cur] = i;
 
+            // 임도연이라면 yCnt를 하나 증가시켜주기
             if (arr[i / 5][i % 5] == 'Y') {
                 comb(cur + 1, i + 1, yCnt + 1);
             } else {
@@ -37,38 +39,48 @@ public class Main {
     static boolean isNear() {
         int cnt = 1;
 
+        // 선택한 좌석인지 체크하기 위해 set에 담음
         for (int i = 0; i < 7; i++) {
             set.add(selected[i]);
         }
 
+        // 좌석 하나 담고 시작하고
         q.offer(selected[0]);
         visited[selected[0]] = true;
 
-        int cur, crow, ccol, mrow, mcol, size;
+        int cur, crow, ccol, mrow, mcol;
         while (!q.isEmpty()) {
             cur = q.poll();
 
+            // 1차원 인덱스 2차원 인덱스로 바꾸기
             crow = cur / 5;
             ccol = cur % 5;
 
+            // 인접한 곳으로만 이동한다.
             for (int dir = 0; dir < 4; dir++) {
                 mrow = crow + delR[dir];
                 mcol = ccol + delC[dir];
 
-
+                // 경계 및 방문 체크
                 if (mrow < 0 || mcol < 0 || 5 <= mrow || 5 <= mcol || visited[mrow * 5 + mcol]) continue;
 
+                // 선택한 좌석인지 체크
                 if (!set.contains(mrow * 5 + mcol)) continue;
 
+                // 인접하면서 선택된 좌석이라면..
                 visited[mrow * 5 + mcol] = true;
                 q.offer(mrow * 5 + mcol);
+                // 인접 개수 증가
                 cnt++;
             }
         }
 
+        // 초기화
         q.clear();
         set.clear();
         Arrays.fill(visited, false);
+
+        // 인접한 개수 cnt가 7개면 return true
         if (cnt == 7) return true;
         else return false;
     }
