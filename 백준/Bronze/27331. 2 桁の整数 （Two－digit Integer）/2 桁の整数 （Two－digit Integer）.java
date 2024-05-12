f3e90b2f0551bae0150bcc1693dca9d6b9433f16
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-05-12 22:41:50 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 88 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine() + br.readLine());
+
+        br.close();
+    }
+}
