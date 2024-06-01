778b6173801332bedc5a8f372a2e4ddcb61d0d0e
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-06-01 22:13:38 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 76 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+
+        if (X % 7 == 2) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
