afb08613788ec092a4871303faf015851d8d2718
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-06-11 23:08:04 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 84 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[][] dp;
+    static final int MOD = 1_000_000_000;
+
+    // 계단수 : 인접한 모든 자리의 차이가 1
+
+    static int recur(int cur, int prv) {
+        if (cur == N) return 1;
+
+        if (dp[cur][prv] != -1) return dp[cur][prv];
+
+        int ret = 0;
+        for (int i = 0; i < 10; i++) {
+            if (cur == 0 && i == 0) continue;
+            if (cur != 0 && Math.abs(prv - i) != 1) continue;
+
+            ret = (ret + recur(cur + 1, i) % MOD) % MOD;
+        }
+
+        dp[cur][prv] = ret;
+
+        return ret % MOD;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        dp = new int[N + 10][10];
+        for (int[] d1 : dp) {
+            Arrays.fill(d1, -1);
+        }
+
+        System.out.println(recur(0, 0));
+
+        br.close();
+    }
+}
