ef20a39477f5bdf43fb3f1e06306c5a0da3b70a3
Hyejoo Jung <81564593+hyejoojung719@users.noreply.github.com>
2024-08-06 21:39:27 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 528 ms, Memory: 52396 KB -BaekjoonHub

---
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, K;
+    static ArrayList<Node>[] lst;
+    static int[] dist;
+    static boolean[] visited;
+    static PriorityQueue<Node> pq = new PriorityQueue<>();
+
+    static class Node implements Comparable<Node> {
+        int to;
+        int cost;
+
+        Node(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node node) {
+            return this.cost - node.cost;
+        }
+    }
+
+    static void daijk(int start) {
+        Arrays.fill(dist, 1 << 30);
+        Arrays.fill(visited, false);
+        dist[start] = 0;
+        pq.offer(new Node(start, 0));
+
+        int cur, nxt, nc;
+        while (!pq.isEmpty()) {
+            cur = pq.poll().to;
+
+            if (visited[cur]) continue;
+
+            visited[cur] = true;
+
+            for (Node node : lst[cur]) {
+                nxt = node.to;
+                nc = dist[cur] + node.cost;
+
+                if (dist[nxt] > nc) {
+                    dist[nxt] = nc;
+                }
+                pq.offer(new Node(nxt, dist[nxt]));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        lst = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            lst[i] = new ArrayList<>();
+        }
+        dist = new int[N + 1];
+        visited = new boolean[N + 1];
+
+        int a, b, c;
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+
+            lst[a].add(new Node(b, c));
+            lst[b].add(new Node(a, c));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        daijk(start);
+
+        System.out.println(dist[end]);
+    }
+}
