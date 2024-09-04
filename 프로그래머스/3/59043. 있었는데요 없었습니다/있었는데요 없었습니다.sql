deee11b557ae653e78ceca17e695bd53f9847e2a
Hyejoo Jung <fixup719@gmail.com>
2024-09-04 17:14:26 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,8 +1,4 @@
--- [문제]
--- 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름 조회
--- ANIMAL_INS : 보호소에 들어온 동물 정보
--- ANIMAL_OUTS : 보호소에서 입양보낸 동물 정보
 SELECT I.ANIMAL_ID, I.NAME
 FROM ANIMAL_INS I JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
 WHERE I.DATETIME > O.DATETIME
-ORDER BY I.DATETIME ASC;
+ORDER BY I.DATETIME;
