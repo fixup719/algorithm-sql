ee2f7775f332385ee4135ea90ff3d53e300c2f17
fixup719 <fixup719@gmail.com>
2024-09-11 17:33:03 +0900

    [Silver III] Title: 계단 오르기, Time: 68 ms, Memory: 11840 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,46 +1,50 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static int N;
+    static int[] arr;
+    static int[][][] dp = new int[310][10][10];
+
+    static int recur(int cur, int use, int pass) {
+        if (cur == N){
+            if (use == 0) return Integer.MIN_VALUE;
+            else return 0;
+        }
+
+        if (dp[cur][use][pass] != -1) return dp[cur][use][pass];
+
+        // 연속해서 3개이상 X
+        int ret = Integer.MIN_VALUE;
+        if (use < 2) {
+            // 아직 연속된 계단이 1개니까 연속으로 오르기 가능
+            ret = Math.max(ret, recur(cur + 1, use + 1, 0) + arr[cur]);
+            // 또는 그냥 건너 뛰기
+            if (pass < 1) ret = Math.max(ret, recur(cur + 1, 0, pass + 1));
+        } else if (pass < 1){
+            // cnt가 이미 2인 경우는 더 이상 연속해서 오를 수 없으므로 건너 뛰기
+            ret = Math.max(ret, recur(cur + 1, 0, pass + 1));
+        }
+
+        dp[cur][use][pass] = ret;
+        return ret;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N + 10];
-        for (int i = 1; i <= N; i++) {
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] dp = new int[N + 10];
-        dp[1] = arr[1];
-        dp[2] = arr[1] + arr[2];
-        // max 값이 누적된다...?
-
-//        dp[3] = Math.max(dp[2] + arr[3], arr[1] + arr[3]);
-        // 근데 여기서 dp[2]+arr[3]이면은 4번째 계단 못 밟음..
-        /*
-            연속 3개 계단을 밟을 수 없음
-            4번째 계단을 밟는다면,
-            1->3->4
-            1->2->4
-            까지 가능하다
-            2->3->4 는 불가능
-
-            dp[N-3] + arr[N-1] + arr[N]
-            dp[N-2] + arr[N]
-        **/
-
-        for (int i = 3; i <= N; i++) {
-            dp[i] = Math.max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]);
+        for (int[][] d1 : dp) {
+            for (int[] d2 : d1) {
+                Arrays.fill(d2, -1);
+            }
         }
-
-        bw.write(String.valueOf(dp[N]));
-        bw.flush();
-        bw.close();
-        br.close();
+        int ret = recur(0, 0, 0);
+        System.out.println(ret);
     }
 }
+
